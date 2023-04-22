81283cfa41471ceda4c69a80f8a6b491ec3f7eb7
Daniel Burger <public@danielburger.online>
2023-04-22 17:22:51 +0200

    Fix devcontainer and add reset scripts

---
+++ typescript/.devcontainer/scripts/reset_devcontainer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# ? Use this utility script to reset the devcontainer to its initial state
+# 1. Open the repo locally, not inside a devcontainer
+# 2. Be sure that the Docker daemon is running
+# 3. Run this script from the root of the microservice
+
+# Delete the packages and store
+rm -rf node_modules
+rm -rf .pnpm-store
+
+# Stop and delete the containers
+docker stop TypeScriptDevcontainer
+docker container rm typescript_devcontainer
+
+# Remove the devcontainer images
+docker image rm mcr.microsoft.com/devcontainers/typescript-node
+docker image rm typescript_devcontainer-devcontainer
+docker image rm typescript_devcontainer-devcontainer
+
+# Remove the devcontainer volumes
+docker volume rm cached-node_modules
+docker volume rm cached-pnpm_store
+docker volume rm vscode
