094546920568d065699390cf9ef3529b46b7ed85
Daniel Burger <public@danielburger.online>
2023-04-22 13:29:49 +0200

    Adjust Python template a bit

---
+++ python/src/example.py
@@ -0,0 +1,5 @@
+"""This is an example Python script"""
+
+def test() -> str:
+    """This is an example function"""
+    return "Hello World"
--- python/src/main.py
+++
@@ -1,8 +0,0 @@
-"""This is an example module"""
-
-print("Hello World")
-
-
-def test() -> int:  # Linting and type checking works
-    """This is an example function"""
-    return 1
