5c74eb763313dfbe0ad2314edf92676395837567
Daniel Burger <public@danielburger.online>
2023-04-22 17:13:42 +0200

    Init new NPM project

--- typescript/.devcontainer/scripts/post_create.sh
+++ typescript/.devcontainer/scripts/post_create.sh
@@ -3,5 +3,9 @@
 # Install pnpm
 npm install pnpm -g
 
+# Modify permissions for the PNPM store and packages
+sudo chmod -R 777 node_modules &&
+  sudo chmod -R 777 .pnpm-store
+
 # Install dependencies
 pnpm install
